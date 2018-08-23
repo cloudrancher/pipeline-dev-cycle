ff430540c5f02772ec491236594f1227618291a9
Eric George <eric@nolab.org>
2018-08-23 11:44:47 -0600

    Initial Commit

---
+++ src/main/java/com/cloudrancher/pipeline/dev/PipelineDevCycleApplication.java
@@ -0,0 +1,12 @@
+package com.cloudrancher.pipeline.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineDevCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineDevCycleApplication.class, args);
+	}
+}
